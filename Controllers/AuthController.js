0d29909ffa4b074d4c820a5bc5d15234e6589afd
barhoumiwajdi <wajdi.barhoumi26@gmail.com>
2023-05-03 15:10:10 +0100

    reset and forget config

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const user = require('../Models/UserModel');
 const nodemailer = require('nodemailer');
+const Token = require('../../Models/Token');
 const email = process.env.EMAIL
 const password = process.env.PASSWORD
 exports.register = async (req, res) => {
@@ -65,91 +66,75 @@ exports.login = async (req, res) => {
     res.status(500).send({ message: 'erreur serveur' || error })
   }
 }
-exports.forgotPassword = async (req, res) => {
+
+
+
+exports.forgetPassword = async (req, res) => {
   try {
-    const { Email } = req.body;
-    const User = await user.findOne({ Email });
-    if (!User) {
-      return res.status(400).send({ message: 'User not found' });
-    } else {
-      const token = jwt.sign({ userId: User._id }, process.env.JWTSECRET, { expiresIn: '15m' });
-
+    const found = await user.findOne({ email: req.body.email });
+    if (found) {
+      const token = await Token.findOne({ companyId: found._id });
+      if (token) {
+        await token.deleteOne()
+      }
       const transporter = nodemailer.createTransport({
-        service: 'gmail',
+        host: 'smtp.gmail.com',
         auth: {
-          user: email,
-          pass: password,
-        },
-      });
-
-      const mailOptions = {
-        from: email,
-        to: Email,
-        subject: 'Réinitialisation de votre mot de passe',
-        html: `
-        <p>Bonjour , pour renitialisez votre mot de passe SVP Cliquez sur le lien ci-dessous :</p>
-        <a  href="http://localhost:3000/demo8/auth-reset">Réinitialiser votre mot de passe</a>
-        <p> vous avez besoin d utiliser cet code : </p>
-       
-        <hr>
-        <p style='color:red;'>
-        ${token} 
-        </p>
-        <hr>
-       
-        <p>Au revoir et merci pour votre confiance </p>
-      `,
-      };
-
-      await transporter.sendMail(mailOptions);
-
-      res.status(200).send({ message: 'Password reset link sent to your email' });
+          user: process.env.EMAIL,
+          pass: process.env.PASSWORD
+        }
+      })
+      const resetToken = randomString.generate(30)
+      const createdToken = await new Token({
+        userId: found._id,
+        token: resetToken,
+      }).save();
+      const resetLink = 'http://localhost:3000/resetPassword/' + resetToken
+      await transporter.sendMail({
+        from: ' EMAIL@gmail.com',
+        to: req.body.email,
+        subject: `Reset password request for ${found.Nom} company`,
+        html: `<h1>You requested to reset your password so here is your reset password link</h1>
+                <p>${resetLink}</p>`
+      })
+      res.send({ message: 'Reset password mail sent successfully.', token: createdToken })
+    } else {
+      res.status(400).send({ message: 'Email not found, please enter a valid e-mail!' })
     }
-
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ message: 'error server' })
   }
 }
 
 exports.resetPassword = async (req, res) => {
-  const { NewPassword } = req.body;
-  const { Code } = req.body
-
   try {
-
-    const decoded = jwt.verify(Code, process.env.JWTSECRET)
-    const userfound = await user.findById(decoded.userId)
-    if (!userfound) {
-      return res.status(404).send({ message: 'User not found' });
+    const token = await Token.findOne({ token: req.params.token })
+    if (token) {
+      const diffDate = new Date() - token.createdAt;
+      const diffSeconds = Math.floor(diffDate / 1000);
+      if (diffSeconds < 3600) {
+        const saltRounds = bcrypt.genSaltSync(10);
+        bcrypt.hash(req.body.password, saltRounds, async (error, hash) => {
+          if (error) {
+            res.status(500).json({ message: 'Server Error' })
+          } else {
+            await Company.findByIdAndUpdate(token.companyId, { password: hash })
+            res.send({ message: 'Password reset successfully!' });
+          }
+        })
+        await Token.findByIdAndRemove(token._id)
+      } else {
+        await Token.findByIdAndRemove(token._id)
+        res.status(400).send({ message: 'Token expired!' });
+      }
+    } else {
+      res.status(400).send({ message: 'Invalid token!' });
     }
-
-    await user.findByIdAndUpdate(userfound._id, { Password: NewPassword })
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: email,
-        pass: password,
-      },
-    });
-
-    const mailOptions = {
-      from: email,
-      to: userfound.Email,
-      subject: 'succés de renitialisation',
-      html: `
-      <p>Bonjour , vous avez renitialisez votre mot de passe avec succes 
-      
-      Au revoir et merci pour votre confiance </p>
-    `,
-    };
-    await transporter.sendMail(mailOptions);
-    res.status(200).send({ message: 'Password reset successfully' });
   } catch (error) {
-
-    res.status(400).send({ message: 'Invalid token' });
+    res.status(500).json({ message: 'error server' })
   }
 }
+
 // exports.LogOut = async (req, res) => {
 //   try {
 
